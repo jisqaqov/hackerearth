f7a2d301986a1a53b155a6d818291b75e1fc853f
Zhandos Iskakov <j.isqaqov@gmail.com>
2019-06-04 21:36:30 +0300

    shortest path understanding problem on graphs;

--- src/graph/ShortestPathProblem.java
+++ src/graph/ShortestPathProblem.java
@@ -78,7 +78,7 @@ public class ShortestPathProblem {
     }
 
     private static void dijkstra(FastReader in, PrintWriter out, int n, int m) {
-        List<Node>[] adjList = new ArrayList[n];
+        Map<Integer, List<int[]>> graph = new HashMap<>();
 
         try {
             for (int i = 0; i < m; i++) {
@@ -86,16 +86,8 @@ public class ShortestPathProblem {
                 int v = in.nextInt() - 1;
                 int w = in.nextInt();
 
-                if (adjList[u] == null) {
-                    adjList[u] = new ArrayList<>();
-                }
-
-                if (adjList[v] == null) {
-                    adjList[v] = new ArrayList<>();
-                }
-
-                adjList[u].add(new Node(v, w));
-                adjList[v].add(new Node(u, w));
+                graph.putIfAbsent(u, new ArrayList<>());
+                graph.get(u).add(new int[] {v, w});
             }
         } catch (Exception e) {
             //
@@ -103,8 +95,8 @@ public class ShortestPathProblem {
 
         Set<Integer> visited = new HashSet<>();
 
-        HeapMap<Node> heap = new HeapMap<>();
-        heap.add(new Node(0, 0));
+        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(node1 -> node1[1]));
+        heap.offer(new int[]{0, 0});
 
         int INFINITE = (int) Math.pow(10, 9);
 
@@ -112,195 +104,30 @@ public class ShortestPathProblem {
         Arrays.fill(dis, INFINITE);
         dis[0] = 0;
 
-        while (heap.getSize() > 0) {
-            Node node = heap.poll();
+        while (!heap.isEmpty()) {
+            int[] node = heap.poll();
+            int u = node[0];
 
-            visited.add(node.vertex);
-
-            if (adjList[node.vertex] == null) {
+            if (visited.contains(u)) {
                 continue;
             }
 
-            for (Node adj : adjList[node.vertex]) {
-                if (visited.contains(adj.vertex)) {
-                    continue;
-                }
+            dis[u] = node[1];
+            visited.add(u);
 
-                int newDis = dis[node.vertex] + adj.cost;
-                if (dis[adj.vertex] > newDis) {
-                    dis[adj.vertex] = newDis;
-                }
+            if (graph.containsKey(u)) {
+                for (int[] adj : graph.get(u)) {
+                    int v = adj[0], w = adj[1];
 
-                heap.add(new Node(adj.vertex, dis[adj.vertex]));
+                    if (!visited.contains(v)) {
+                        heap.offer(new int[]{v, Math.min(dis[u] + w, dis[v])});
+                    }
+                }
             }
         }
 
         for (int t = 1; t < n; t++) {
             out.print(dis[t] + " ");
-        }
-    }
-
-    private static class HeapMap<T extends Comparable> {
-        private static final int INITIAL_CAPACITY = 10;
-        private Node[] heap = new Node[INITIAL_CAPACITY];
-        private int size = 0;
-        Map<Integer, Node> map = new HashMap<>();
-
-        private int getLeftChildIndex(int parentIndex) {
-            return 2 * parentIndex + 1;
-        }
-
-        private int getRightChildIndex(int parentIndex) {
-            return 2 * parentIndex + 2;
-        }
-
-        private int getParentIndex(int index) {
-            return (index - 1) / 2;
-        }
-
-        @SuppressWarnings("unchecked")
-        private T getParent(int index) {
-            return (T) heap[getParentIndex(index)];
-        }
-
-        @SuppressWarnings("unchecked")
-        private T leftChild(int parentIndex) {
-            return (T) heap[getLeftChildIndex(parentIndex)];
-        }
-
-        @SuppressWarnings("unchecked")
-        private T rightChild(int parentIndex) {
-            return (T) heap[getRightChildIndex(parentIndex)];
-        }
-
-        private boolean hasParent(int childIndex) {
-            return getParentIndex(childIndex) >= 0;
-        }
-
-        private boolean hasLeftChild(int parentIndex) {
-            return size > getLeftChildIndex(parentIndex);
-        }
-
-        private boolean hasRightChild(int parentIndex) {
-            return size > getRightChildIndex(parentIndex);
-        }
-
-        @SuppressWarnings("unchecked")
-        public Node peek() {
-            if (size == 0)
-                throw new IllegalArgumentException("Empty Heap");
-
-            return heap[0];
-        }
-
-        @SuppressWarnings("unchecked")
-        public Node poll() {
-            if (size == 0)
-                throw new IllegalArgumentException("Empty Heap");
-
-            Node poll = heap[0];
-
-            map.remove(poll.vertex);
-
-            heap[0] = heap[size - 1];
-            size--;
-            siftDown();
-
-            return poll;
-        }
-
-        public void add(Node item) {
-            if (size >= this.heap.length)
-                this.growHeap(size + 1);
-
-            heap[size] = item;
-
-            map.put(item.vertex, item);
-
-            size++;
-
-            siftUp();
-        }
-
-        private void growHeap(int rate) {
-            int capacity = this.heap.length;
-            int newCapacity = capacity + (capacity < 64 ? capacity + 2 : capacity >> 1);
-            if (newCapacity - 2147483639 > 0) {
-                newCapacity = hugeCapacity(rate);
-            }
-
-            this.heap = Arrays.copyOf(this.heap, newCapacity);
-        }
-
-        private int hugeCapacity(int capacity) {
-            if (capacity < 0) {
-                throw new OutOfMemoryError();
-            } else {
-                return capacity > 2147483639 ? 2147483647 : 2147483639;
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        private void siftDown() {
-            int currentIndex = 0;
-            Node current = heap[0];
-
-            while (hasLeftChild(currentIndex)) {
-                int nextIndex = getLeftChildIndex(currentIndex);
-                int rightChildIndex = getRightChildIndex(currentIndex);
-
-                if (hasRightChild(currentIndex) && rightChild(currentIndex).compareTo(heap[nextIndex]) > 0)
-                    nextIndex = rightChildIndex;
-
-                if (current.compareTo(heap[nextIndex]) < 0) {
-                    swap(currentIndex, nextIndex);
-
-                    currentIndex = nextIndex;
-                    current = heap[currentIndex];
-                }
-                else
-                    break;
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        private void siftUp() {
-            Node current = heap[size - 1];
-            int currentIndex = size - 1;
-            while (hasParent(currentIndex) && current.compareTo(getParent(currentIndex)) > 0) {
-                int parentIndex = getParentIndex(currentIndex);
-                swap(currentIndex, parentIndex);
-
-                currentIndex = parentIndex;
-                current = heap[parentIndex];
-            }
-        }
-
-        private void swap(int index1, int index2) {
-            Node temp = heap[index2];
-            heap[index2] = heap[index1];
-            heap[index1] = temp;
-        }
-
-        public int getSize() {
-            return size;
-        }
-    }
-
-    private static class Node implements Comparable {
-        int vertex, cost;
-
-        public Node() {
-        }
-
-        public Node(int vertex, int cost) {
-            this.vertex = vertex;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            return cost - ((Node)o).cost;
         }
     }
 
